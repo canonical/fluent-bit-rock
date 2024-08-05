b552eae85c65adea8691bc92e3f169f2fac638a6
Maciej Golaszewski <maciej.golaszewski@canonical.com>
2024-08-05 14:02:17 +0200

    warning for skipped test

--- tests/sanity/test_rock.py
+++ tests/sanity/test_rock.py
@@ -3,16 +3,23 @@
 #
 
 import pytest
+import warnings
 from k8s_test_harness.util import docker_util, env_util
 
 
 @pytest.mark.parametrize("image_version", ("2.1.6", "1.9.5"))
 def test_sanity(image_version):
-    rock = env_util.get_build_meta_info_for_rock_version(
-        "fluent-bit", image_version, "amd64"
-    )
-    image = rock.image
+    try:
 
-    entrypoint = "fluent-bit"
-    process = docker_util.run_in_docker(image, [entrypoint, "--version"])
-    assert f"Fluent Bit v{image_version}" in process.stdout
+        rock = env_util.get_build_meta_info_for_rock_version(
+        "fluent-bit", image_version, "amd64"
+        )
+    except ValueError:
+        warnings.warn(UserWarning(f"image version {image_version} not found"))
+
+    else:
+        image = rock.image
+
+        entrypoint = "fluent-bit"
+        process = docker_util.run_in_docker(image, [entrypoint, "--version"])
+        assert f"Fluent Bit v{image_version}" in process.stdout
